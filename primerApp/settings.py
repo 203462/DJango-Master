4392cefef6be94e0adb6ca6ab26a7857702dfad6
203462 <203462@ids.upchiapas.edu.mx>
2022-01-27 02:19:19 -0600

    update settings

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -38,16 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #Componentes agregados
+    'primerComponente'
+    'Login',
+    'Registro',
+
     #Librerias para el proyecto
      'rest_framework',
+     'rest_framework.authtoken',
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 MIDDLEWARE = [
